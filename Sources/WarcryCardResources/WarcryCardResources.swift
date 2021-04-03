91b231c441f78d2c2ecdfde0ded60aa1033a3d00
Mario Guerrieri <mario@guerrieri.codes>
2021-04-03 13:29:46 -0700

    Attempt to provide access to a resource via this struct?

---
+++ Sources/WarcryCardResources/WarcryCardResources.swift
@@ -0,0 +1,13 @@
+//
+//  WarcryCardResources.swift
+//  
+//
+//  Created by Mario Guerrieri on 4/3/21.
+//
+
+import SwiftUI
+
+struct WarcryCardResources {
+    @available(iOS 13.0, *)
+    static let weaponProfile = Image("weapon-profile", bundle: .module)
+}
