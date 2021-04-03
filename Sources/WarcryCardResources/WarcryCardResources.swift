ad0b2e196a4fa64cb61c62dc19458f0b8ae8af47
Mario Guerrieri <mario@guerrieri.codes>
2021-04-03 13:34:37 -0700

    This should probably be `public`, huh?

--- Sources/WarcryCardResources/WarcryCardResources.swift
+++ Sources/WarcryCardResources/WarcryCardResources.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct WarcryCardResources {
+public struct WarcryCardResources {
     @available(iOS 13.0, watchOS 6.0, tvOS 13.0, OSX 10.15, *)
-    static let weaponProfile = Image("weapon-profile", bundle: .module)
+    public static let weaponProfile = Image("weapon-profile", bundle: .module)
 }
