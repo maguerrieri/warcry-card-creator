157bbb6eb4ff44afb3cdfe9c3302e952deabd85f
Mario Guerrieri <mario@guerrieri.codes>
2021-04-03 13:33:22 -0700

    Add availability annotations for all platforms

--- Sources/WarcryCardResources/WarcryCardResources.swift
+++ Sources/WarcryCardResources/WarcryCardResources.swift
@@ -8,6 +8,6 @@
 import SwiftUI
 
 struct WarcryCardResources {
-    @available(iOS 13.0, *)
+    @available(iOS 13.0, watchOS 6.0, tvOS 13.0, OSX 10.15, *)
     static let weaponProfile = Image("weapon-profile", bundle: .module)
 }
